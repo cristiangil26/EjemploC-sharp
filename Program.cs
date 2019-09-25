d948b3654d9d030e696d76ff2f6f83b8c2891673
cristian gil <cgil@aplyca.com>
2019-09-25 11:34:15 -0500

    Ejemplo de aplicación de escritorio c#

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        private int[,,] mat;
+
+        public void Cargar(int n)
+        {
+            
+            mat = new int[n, n,n];
+            for (int x = 0; x < n; x++)
+            {
+                for (int y = 0; y < n; y++)
+                {
+                    for (int z = 0; z < n; z++)
+                    {
+                        mat[x, y, z] = 0;
+                    }
+                    
+                }
+            }
+        }
+
+        public void Imprimir()
+        {
+            for (int x = 0; x < mat.GetLength(0); x++)
+            {
+                for (int y = 0; y < mat.GetLength(1); y++)
+                {
+                    for (int z = 0; z < mat.GetLength(2); z++)
+                    {
+                        Console.Write(mat[x, y,z] + " ");
+                    }
+                        
+                }
+                Console.WriteLine();
+            }
+        }
+        static void Main(string[] args)
+        {
+            Program ma = new Program();
+            ma.Cargar(3);
+            ma.Imprimir();
+            Console.ReadKey();
+        }
+    }
+}
